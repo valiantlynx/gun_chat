985a4b28ac936e07d7dd9f17a9287a6136ff699a
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-03-12 10:07:02 +0000

    inti

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', "ejs");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+  });
+  
+
+app.listen(3000, function(req, res) {
+    console.log("Server is running successfully");
+});
