2e8358b684edd0f621964af263ccc4ee9b8454e4
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-03-14 16:47:02 +0100

    Update install_gun.sh

--- install_gun.sh
+++ install_gun.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/amark/gun.git
 cd gun
 docker build -t myrepo/gundb:v1 .
-docker run -d -p 8765:8765 myrepo/gundb:v1
+docker run -d --name  gun_chat_server -p 8765:8765 myrepo/gundb:v1
