23b7f4a460226329021dd2a846c2e8efe33433b6
valiantlynx <gormerykombo@gmail.com>
2023-03-14 17:31:36 +0100

    dfd

--- install_gun.sh
+++ install_gun.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# remove old gun
+ls
+sudo docker stop gun_chat_server
+sudo docker system prune -a
+ls
+sudo rm -rf gun
+sudo docker ps -a
+sudo docker images -a
+
+# install gun
 git clone https://github.com/amark/gun.git
 cd gun
 docker build -t gun .
