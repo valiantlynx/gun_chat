cf6b701921950fb9d00d54cd095f36e0f8835dcd
valiantlynx <gormerykombo@gmail.com>
2023-03-16 18:52:25 +0100

    docker

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# remove old gun
+# remove old gun_chat
 ls
 docker stop gun_chat
 docker system prune -af
@@ -10,7 +10,7 @@ rm -rf gun_chat
 docker ps -a
 docker images -a
 
-# install gun
+# install gun_chat
 git clone https://github.com/valiantlynx/gun_chat.git
 cd gun_chat
 docker build -t gun_chat .
