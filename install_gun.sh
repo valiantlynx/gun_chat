9ffac568a519643876c56f86a12f557a81ace49a
valiantlynx <gormerykombo@gmail.com>
2024-04-07 14:41:51 +0200

    ok

--- install_gun.sh
+++ install_gun.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-
-# remove old gun
-ls
-docker stop gun
-docker system prune -af
-
-ls
-rm -rf gun
-docker ps -a
-docker images -a
-
 # install gun
 git clone https://github.com/amark/gun.git
-cd gun
-docker build -t gun .
-docker run -d --name gun -p 8765:8765 gun
+rm -rf gun/.git
+docker-compose up -d
