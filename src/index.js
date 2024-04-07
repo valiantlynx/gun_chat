0d51047f20576fc32529ab7624a2203dbb015aba
valiantlynx <gormerykombo@gmail.com>
2024-04-07 18:35:26 +0200

    Squashed 'gun/' content from commit 1c095b1
    git-subtree-dir: gun
    git-subtree-split: 1c095b13e9226c3090a17f0e7d5bf41b82926abc

---
+++ src/index.js
@@ -0,0 +1,20 @@
+;(function(){
+
+var Gun = require('./root');
+require('./shim');
+require('./onto');
+require('./book');
+require('./valid');
+require('./state');
+require('./dup');
+require('./ask');
+require('./core');
+require('./on');
+require('./map');
+require('./set');
+require('./mesh');
+require('./websocket');
+require('./localStorage');
+module.exports = Gun;
+	
+}());
